c92f114185a260847622a3e3ec03ed9a96f2e6c2
desta-study <masterenko1to@mail.ru>
2017-10-04 15:13:31 +0300

    Update empty.cpp

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -1,5 +1,18 @@
-#include "stack.hpp"
+#include <iostream>
+#include "Stack.hpp"
+using namespace std;
+
+void stackEmpty() {
+	cout << endl << "---empty.cpp---" << endl;
+	Stack<char> arrayChar;
+	char wordChar[] = "KCATS_RAHC";
+	for(int i = 0; i < strlen(wordChar); i++) { arrayChar.push(wordChar[i]); }
+	while(!arrayChar.empty()) {
+		cout << arrayChar.pop() << " ";
+	}
+	cout << endl << endl;
+}
 
 int main() {
-	stack_empty(4);
+	stackEmpty();
 }
