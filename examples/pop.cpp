bc9d05dbfe816b86e21d57b1230d312c2b53bc64
desta-study <masterenko1to@mail.ru>
2017-10-04 15:18:21 +0300

    Update pop.cpp

--- examples/pop.cpp
+++ examples/pop.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
 #include "stack.hpp"
 
-int main() {
-	stack_pop(3);
+using namespace std;
+
+void stackPop() 
+{
+	Stack <int> arrayInt;
+	for(int i = 0; i < 10; i++) 
+		arrayInt.push(i); 
+	for(int j = 0; j < 6; j++) 
+		cout << arrayInt.pop() << " "; 
+	cout << endl;
+}
+
+int main() 
+{
+	stackPop();
 }
