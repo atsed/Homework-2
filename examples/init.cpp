b276478c7e4a14aad335f1caf938d5b5ec2120af
desta-study <masterenko1to@mail.ru>
2017-10-04 15:14:34 +0300

    Update init.cpp

--- examples/init.cpp
+++ examples/init.cpp
@@ -1,6 +1,14 @@
 
+#include <iostream>
 #include "stack.hpp"
+using namespace std;
+
+void stackInit() 
+{
+	Stack<int> arrayInt;
+	cout << "arrayInt created" << endl;
+}
 
 int main() {
-	stack_init();
+	stackInit();
 }
