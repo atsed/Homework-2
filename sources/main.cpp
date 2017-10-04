da9d8704cbbdb2292f027ed27476aec3b4b9aff2
desta-study <masterenko1to@mail.ru>
2017-10-04 15:33:38 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,45 +1,68 @@
+#include <iostream>
 #include "stack.hpp"
 
-int s = 0;
+using namespace std;
 
-void stack_init() {
-	Stack<int> st;
-	cout << "Created stack" << st << endl;
-}
-
-void stack_push(size_t n) {
-	Stack<int> st;
-	for(int i = 0; i<n; i++) {
-		st.push(i);
-	}
-	cout << "Created stack" << " " << n << endl;
-}
-
-void stack_pop(size_t n) {
-	Stack<int> st;
-	for(int i = 0; i<n; i++) {
-		st.push(i);
-	}
+int main() 
+{
+	try 
+	{
+		Stack<int> Stack_int(10); 
 		
-	for(int j = 0; j<(n/2); j++) {
-		cout << st.pop() << " ";
-	}
-	cout << endl;
+		Stack_int.push(0);
+		Stack_int.push(1);
+		Stack_int.push(2);
+		Stack_int.push(3);
+		Stack_int.push(4);
+		Stack_int.push(5);
+		Stack_int.push(6);
+		Stack_int.push(7);
+		Stack_int.push(8);
+		Stack_int.push(9);
 
-	cout << "Created stack" << endl;
-	cout << "Push" << n << endl;
-	cout << "Pop" << (n/2) << endl;
+		for(int i = 0; i < 7; i++) 
+		{
+			cout << Stack_int.pop() << endl;
+		}
+		cout << endl << endl;
+
+		cout << "!Элементы Стека типа Char!" << endl;
+		Stack<char> Stack_char(5); 
+		
+   		Stack_char.push('o');
+		Stack_char.push('l');
+		Stack_char.push('l');
+		Stack_char.push('e');
+		Stack_char.push('h');
+
+		for(int i = 0; i < 5; i++) 
+		{
+			cout << Stack_char.pop();
+		}
+		cout << endl;
+
+    		cout << "!Элементы стека типа Char с добавленной памятью!" << endl;
+		Stack<char> Stack_char_OW(5); 
+   
+    		Stack_char_OW.push('e');
+		Stack_char_OW.push('d');
+		Stack_char_OW.push('c');
+		Stack_char_OW.push('b');
+		Stack_char_OW.push('a');
+
+    
+    		for(int i = 0; i < 11; i++) 
+		{
+			cout << Stack_char_OW.pop();
+		}
+		cout << endl;
+
+	} 
+	catch(const exception& e) 
+	{ 
+		cout << e.what() << endl;
+	}
 }
 
-void stack_empty(size_t n) {
-	Stack<int> st;
-	for(int i = 0; i<n; i++) {
-		st.push(i);
-	}
 
-	while(!st.empty()) {
-		cout << st.pop() << endl;
-	}
-	cout << "Created stack" << endl;
-	cout << "Push " << n << endl;
-}
+
